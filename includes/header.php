802eb911c56da4a0b7a72d8ceb23e868cf4178c5
yugeshpandey22 <PARIPANDEY2031@GMAIL.COM>
2026-08-18 13:43:56 +0530

    Make Blood Checkup nav item text color red

--- includes/header.php
+++ includes/header.php
@@ -199,7 +199,7 @@
                                 </li>
                             <?php else: ?>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle text-dark text-uppercase px-3" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <a class="nav-link dropdown-toggle text-uppercase px-3 <?= (strtolower($cat['name']) === 'blood checkup') ? 'text-danger fw-bold' : 'text-dark' ?>" <?= (strtolower($cat['name']) === 'blood checkup') ? 'style="color: red !important;"' : '' ?> href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                         <?= htmlspecialchars($cat['name']) ?>
                                         <?php if (isset($grouped_items[$cat['id']]) && count($grouped_items[$cat['id']]) > 0): ?>
                                             <i class="fa-solid fa-chevron-down ms-1" style="font-size: 0.8em;"></i>
