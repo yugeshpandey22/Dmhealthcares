c4ed27a2d432c9aa314b23c1322323957a497795
yugeshpandey22 <PARIPANDEY2031@GMAIL.COM>
2026-09-08 00:03:35 +0530

    Remove unused sync_database.php and update admin navbar manager

--- admin/navbar_manager.php
+++ admin/navbar_manager.php
@@ -188,9 +188,6 @@ if ($current_cat_id) {
             </p>
         </div>
         <div class="d-flex gap-2">
-            <a href="../backend/sync_database.php" target="_blank" class="btn btn-outline-primary rounded-pill px-3 fw-semibold btn-sm" title="Sync database categories & links">
-                <i class="fa-solid fa-arrows-rotate me-1"></i> Sync DB
-            </a>
             <?php if($current_category): ?>
                 <a href="navbar_manager.php" class="btn btn-outline-dark rounded-pill px-4 fw-semibold btn-sm">
                     <i class="fa-solid fa-arrow-left me-1"></i> All Categories
